ab5a91282fb110717ca92d4277b1cf65b439a8dc
Muukeli <siponenteemu@gmail.com>
2022-11-24 03:27:08 +0200

    Create player's selection string
    - player's selection still needs to be translated from string to number

--- myScript.js
+++ myScript.js
@@ -1,7 +1,12 @@
 const computerSelection = getComputerChoice();
-
+const playerSelection = getPlayerChoice();
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
     return computerChoice;
+}
+
+function getPlayerChoice() {
+    playerChoice =  prompt("choose between rock, paper or scissors by typing it.").toLowerCase().trim();
+    
 }
